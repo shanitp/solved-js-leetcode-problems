e3c2aa81f664558248bd2922b700a830d1b716c8
Shani Thalappully Preman <shanitpreman@gmail.com>
2025-03-17 15:06:50 -0400

    matrix rotate 90 degree question added

--- matrix.js
+++ matrix.js
@@ -1,3 +1,15 @@
+// This JavaScript function rotate(matrix) rotates an n×n 2D matrix 90 degrees clockwise in-place. 
+// For example, given:
+// 1  2  3
+// 4  5  6
+// 7  8  9
+
+// After transposition, it becomes:
+// 1  4  7
+// 2  5  8
+// 3  6  9
+
+
 function rotate(matrix) {
     const n = matrix.length;
     
--- move-zeros.js
+++ move-zeros.js
@@ -12,7 +12,24 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-var moveZeroes = function(nums) {
-   console.log("nums", nums)
 
+nums = [0, 1, 0, 3, 12]
+
+var moveZeroes = function (nums) {
+   let nonZeroIndex = 0
+   for (i = 0; i < nums.length; i++) {
+      if (nums[i] !== 0) {
+         console.log("not zero", nums[i]);
+         [nums[nonZeroIndex], nums[i]] = [nums[i], nums[nonZeroIndex]]
+         nonZeroIndex++;
+      }
+   }
 };
+moveZeroes(nums);
+console.log(nums);
+
+
+nums = [0, 1, 0, 3, 12];
+moveZeroes(nums);
+console.log(nums);  // Output: [1, 3, 12, 0, 0]
+
